72daf04dd33551b038577ab2582875182f24cdb1
Zonder <f-m@bk.ru>
2018-12-19 15:30:45 +0300

    First Steps
    Added: css, file_read_sync, run_terminal.

---
+++ scripts/file_man.js
@@ -0,0 +1,19 @@
+function ReadFileSync (file) {
+  var fs = require('fs');
+  return fs.readFileSync(file, 'utf8');
+}
+
+function RunExec () {
+  const { exec } = require('child_process');
+  exec('date', (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    ExecReturn (`stdout: ${stdout}`);
+  });
+}
