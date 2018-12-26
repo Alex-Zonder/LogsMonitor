273842abdd5d55145e7b5c68a68ac2119075d1de
Zonder <f-m@bk.ru>
2018-12-26 16:42:04 +0300

    Win support

--- scripts/system.js
+++ scripts/system.js
@@ -3,6 +3,16 @@ function ToJson (data) {return JSON.stringify(data);}
 function FromJson (data) {return JSON.parse(data);}
 
 
+//     OS     //
+//  'darwin', 'freebsd', 'linux', 'sunos' or 'win32'. //
+var pach_separator = "/";
+var pach_root = "/";
+var os = require('os');
+var platform = os.platform();
+if (platform == 'win32') {
+	pach_separator = "\\";
+	pach_root = "C:\\";
+}
 
 
 //      Shell Exec      //
